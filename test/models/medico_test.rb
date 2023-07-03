8717999d23f506d5b5a1206a266f5c89f45484ac
Giudicelli <giudicellielias@gmail.com>
2023-07-03 17:58:43 -0300

    Teste de model, teste de nedico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -4,4 +4,19 @@ class MedicoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'Salvando medico' do
+    medico = Medico.new nome: 'Guilherme Elias',
+                        cpf:'15146076030',
+                        email:'guilherme@gmail.com',
+                        especialidade:'Especialista',
+                        CRM:'123857AL'
+    assert medico.save
+  end
+
+  test 'Criando cadastro de medico sem preencher dados' do
+    medico = Medico.new
+    assert_not medico.save
+  end
+
 end
